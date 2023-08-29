78e7ad0e6a02e7d62dc24542436ff42d18a6f673
MR. U7P4L 1N <123867620+U7P4L-IN@users.noreply.github.com>
2023-08-29 20:10:00 +0600

    Delete script directory

--- script/app.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js')
-        .then((reg) => console.log('service worker registered', reg))
-        .catch((err) => console.log('service worker not registered', err));
-}
